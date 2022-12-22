ca103c19882ee9c10569b7f1968d128374f7d58f
Witthawat  Tanoi <38246228+endokagi@users.noreply.github.com>
2022-12-22 17:09:55 +0700

    updated header and footer

--- App.js
+++ App.js
@@ -2,10 +2,14 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var route = require('./route');
+var favicon = require('serve-favicon');
 
 app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(express.static("assets/img"));
+app.use(favicon(__dirname + '/VestALife.ico'));
 app.use(route);
 
 app.listen(app.get("port"));
