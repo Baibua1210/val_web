9a123f089a623d4853487fd251cde82084d6d00f
Baibua1210 <56666179+Baibua1210@users.noreply.github.com>
2022-12-22 13:50:57 +0700

    add dropdown menus

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
 });
 
 app.set("port", process.env.PORT || 3000);
-app.set("view", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(route);
 
